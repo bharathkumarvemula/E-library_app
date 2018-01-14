5c3ed2e61cf8bd418dc84005283bbc5ffe2f1d11
bharathkumarvemula <35340282+bharathkumarvemula@users.noreply.github.com>
2018-01-14 16:20:35 -0600

    Add files via upload

---
+++ savedata.php
@@ -0,0 +1,16 @@
+<?php
+$file_name = date("U").".pdf/"; //name of your file
+$server_path = "/Applications/XAMPP/xamppfiles/htdocs/Educations"; //server path to folder
+$web_path = "http://192.168.0.19/Educations/"; //web path to folder
+
+$file = $server_path.$file_name;
+file_put_contents($file,"");
+
+$fp = fopen("php://input", 'r');
+while ($buffer =  fread($fp, 8192)) {
+    file_put_contents($file,$buffer,FILE_APPEND | LOCK_SH : 0);
+}
+
+echo $web_path.$file_name;
+?>
+
